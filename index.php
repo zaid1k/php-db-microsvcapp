37b3cb2555b0b9a7f5308f68daccbf4a7876edda
DHOPAWNKAR Zaid OBS/IZ <zaid1kd@gmail.com>
2024-04-04 00:31:57 +0530

    new

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Docker Sample App</title>
+    <title>Docker Sample App</title>
 </head>
 <body>
 <?php
-if($_SERVER['REQUEST_METHOD'] == "POST")
-{
+if($_SERVER['REQUEST_METHOD'] == "POST") {
     $servername = "mysql"; // Assuming you're using a MySQL container named 'mysql'
     $username = "root";
     $password = "password";
     $dbname = "mydatabase";
     $name = $_POST["name"];
     $phone = $_POST["phone"];
-    
+
     // Create Connection
     $conn = new mysqli($servername, $username, $password, $dbname);
-    
+
     // Check connection
     if ($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
     }
-    
+
     // Prepare and execute SQL statement
     $stmt = $conn->prepare("INSERT INTO emp (name, phone) VALUES (?, ?)");
     $stmt->bind_param("ss", $name, $phone);
@@ -30,7 +29,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     } else {
         echo "Error: " . $stmt->error;
     }
-    
+
     // Close connection
     $stmt->close();
     $conn->close();
@@ -39,7 +38,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 <form action="index.php" method="POST">
     <input type="text" name="name">
     <input type="text" name="phone">
-    <input type="submit" name="submit">
+    <input type="submit" name="submit" value="Submit">
 </form>
 </body>
 </html>
