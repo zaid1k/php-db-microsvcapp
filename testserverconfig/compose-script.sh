b6615ad4fb96902921365ae86cd0ba4322937a8c
DHOPAWNKAR Zaid OBS/IZ <zaid1kd@gmail.com>
2024-04-05 00:56:17 +0530

    gg

--- testserverconfig/compose-script.sh
+++ testserverconfig/compose-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo DOCKER_PHP_IMAGE=$1 docker-compose -f /home/ec2-user/testserverconfig/docker-compose.yml up -d
+sudo DOCKER_PHP_IMAGE=$1 /usr/local/bin/docker-compose -f /home/ec2-user/testserverconfig/docker-compose.yml up -d
 
 
 
